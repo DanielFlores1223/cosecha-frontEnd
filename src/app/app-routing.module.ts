805954f1d7d4aa512e91977585d425e2559ba922
DanielFlores1223 <dany-232000@hotmail.com>
2021-10-14 00:31:28 -0500

    creación de componentes vista principal + inicio de menu principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// **Componentes**
+// --VistaPrincipal--
+import { InicioComponent } from './components/vistaPrincipal/inicio/inicio.component';
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent }, 
+  {path:'', redirectTo: 'inicio', pathMatch: 'full'},
+  {path:'**', redirectTo: 'inicio', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/vistaPrincipal/menu/menu.component';
+import { InicioComponent } from './components/vistaPrincipal/inicio/inicio.component';
+import { NosotrosComponent } from './components/vistaPrincipal/nosotros/nosotros.component';
+import { RegistroComponent } from './components/vistaPrincipal/registro/registro.component';
+import { InicioSesionComponent } from './components/vistaPrincipal/inicio-sesion/inicio-sesion.component';
+import { PiePaginaComponent } from './components/vistaPrincipal/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InicioComponent,
+    NosotrosComponent,
+    RegistroComponent,
+    InicioSesionComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
